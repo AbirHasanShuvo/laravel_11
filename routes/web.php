7735b8e2e48edde6fb23e9ff24dad0206601a4c6
Md. Abir Hasan <abirhasanshuvo789@gmail.com>
2025-12-09 15:46:43 +0600

    route naming is completed

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // return view('welcome');
-    return 'Hello, World!';
+    return view('welcome');
+    // return 'Hello, World!';
 });
 
 Route::get('/about', function () {
     return 'This is the about page.';
-});
+})->name('about');
 
 Route::get('/user/{id}/{slug}', function ($id, $slug) {
     return 'Hello User! ' . $id . '-' .  $slug;
-});
+})->name('user');
