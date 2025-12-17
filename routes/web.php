3aedf0e75804a05b5fc1ff28f380117eed0c8548
Md. Abir Hasan <abirhasanshuvo789@gmail.com>
2025-12-17 16:41:42 +0600

    routing group is completed

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 
-
-
 Route::get('/', function () {
     return view('welcome');
     // return 'Hello, World!';
@@ -18,9 +16,21 @@ Route::get('/user/{id}/{slug}', function ($id, $slug) {
     return 'Hello User! ' . $id . '-' .  $slug;
 })->name('user');
 
+
+
 //for grouping routes
-Route::group([], function () {
-    Route::get('/blog/create', function () {
-        return 'Blog Create Page';
-    });
+Route::group(['prefix' => 'blog', 'as' => 'blog.'], function () {
+    //because of i am using prefix here there is no need to add /blog in the route it will by default add it
+    //because of this  'as' => 'blog.' in the name of the route like - name('create') it added blog.create
+    Route::get('/create', function () {
+        return 'blog create page';
+    })->name('create');
+
+    Route::get('/edit', function () {
+        return 'blog edit page';
+    })->name('edit');
+
+    Route::get('/show', function () {
+        return 'blog show page';
+    })->name('show');
 });
